c65d1f3184ce27d853ea77dd4b2577fea198c643
kurulerdem <kurulerdem1@gmail.com>
2020-08-27 09:41:42 +0300

    first commit

---
+++ Interfaces/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace Interfaces
+{
+    public interface IEmployee
+    {
+        void Add();
+        void ShowSalary();
+        void ShowTitle();
+    }
+
+    class Worker: IEmployee
+    {
+        void IEmployee.Add()
+        {
+            Console.WriteLine("Workers Added !");
+        }
+        void IEmployee.ShowSalary()
+        {
+            Console.WriteLine("Workers Salary 1300$");
+        }
+        void IEmployee.ShowTitle()
+        {
+            Console.WriteLine("Title : Worker");
+        }
+    }
+
+    class Manager: IEmployee
+    {
+        void IEmployee.Add()
+        {
+            Console.WriteLine("Manager Added !");
+        }
+        void IEmployee.ShowSalary()
+        {
+            Console.WriteLine("Managers Salary 4300$");
+        }
+        void IEmployee.ShowTitle()
+        {
+            Console.WriteLine("Title : Manager");
+        }
+    }
+
+    class ManageEmployee
+    {
+        public void Add(IEmployee employee)
+        {
+            employee.Add();
+        }
+        public void ShowSalary(IEmployee employee)
+        {
+            employee.ShowSalary();
+        }
+        public void ShowTitle(IEmployee employee)
+        {
+            employee.ShowTitle();
+        }
+
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Proje Ana Dizini
+
+            ManageEmployee manageEmployee = new ManageEmployee();
+            manageEmployee.ShowTitle(new Worker());
+            Console.ReadLine();
+        }
+    }
+}
